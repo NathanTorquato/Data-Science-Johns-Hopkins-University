63a28d073e0bbebd0d7b1dab8030b0bb97703c8a
Nathan Mimoso <nathanmimoso@hotmail.com>
2020-08-29 00:52:40 -0300

    Novo gráfico com a evolução dos homicídios por Estado entre 2000 e 2017.

--- homicidios.R
+++ homicidios.R
@@ -67,13 +67,57 @@ max(df2$periodo)
 ggplot(subset(df2, df2$periodo == '2000'), aes(nome))+
   geom_bar(aes(weight=valor))+
   labs(x="", y="")+
-  ggtitle("Homicídios no Brasil por Estado (2000)", subtitle="Fonte: Atlas da Violência 2020")
+  ggtitle("Homicídios no Brasil por Estado (2000)", subtitle="Fonte: Atlas da Violência 2020")+
+  ylim(c(0,15000))
 
 ggplot(subset(df2, df2$periodo == '2017'), aes(nome))+
   geom_bar(aes(weight=valor))+
   labs(x="", y="")+
   ggtitle("Homicídios no Brasil por Estado (2017)", subtitle="Fonte: Atlas da Violência 2020")
 
+ggplot(subset(df2, df2$periodo == '2017'), aes(nome))+
+  geom_bar(aes(weight=valor))+
+  labs(x="", y="")+
+  ggtitle("Homicídios no Brasil por Estado (2017)", subtitle="Fonte: Atlas da Violência 2020")+
+  ylim(c(0,15000))
 
+library(patchwork)
 
+a <- ggplot(subset(df2, df2$periodo == '2000'), aes(nome))+
+  geom_bar(aes(weight=valor))+
+  labs(x="", y="")+
+  ggtitle("Homicídios no Brasil por Estado (2000)", subtitle="Fonte: Atlas da Violência 2020")+
+  ylim(c(0,15000))
 
+b <- ggplot(subset(df2, df2$periodo == '2017'), aes(nome))+
+  geom_bar(aes(weight=valor))+
+  labs(x="", y="")+
+  ggtitle("Homicídios no Brasil por Estado (2017)", subtitle="Fonte: Atlas da Violência 2020")+
+  ylim(c(0,15000))
+
+a+b
+
+#
+df2000 <- subset(df2, df2$periodo == '2000')
+df2017 <- subset(df2, df2$periodo == '2017')
+
+#Os únicos estados onde a violência diminuiu entre 2000 e 2017 foram SP, RJ e DF
+ggplot(mapping = aes(nome, weight=valor))+
+  geom_bar(data = df2000, fill='steelblue', position='stack')+
+  geom_bar(data = df2017, width=.5, position=position_dodge())
+
+df3 <- rbind(df2000, df2017)
+
+df3$periodo[df3$periodo == 'Ano 2000'] <- '2000 '
+df3$periodo[df3$periodo == 'Ano 2017'] <- '2017 '
+
+#Só para aprender a separar, mas não é isso que eu quero
+ggplot(df3, aes(nome))+
+  geom_bar(aes(weight=valor, fill=regiao))
+
+#Isso era o que eu queria
+ggplot(df3, aes(nome))+
+  geom_bar(aes(weight=valor, fill=periodo), position='dodge')+
+  labs(x="", y="", fill="")+
+  ggtitle("Evolução do número de homicídios no Brasil entre 2000 e 2017", subtitle = "Fonte: Atlas da Violência 2020")+
+  ylim(c(0,15000))
